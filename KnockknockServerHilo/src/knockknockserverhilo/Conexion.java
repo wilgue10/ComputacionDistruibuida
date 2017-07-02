28c4dec2f068ce19210276120a8b0087c5053210
DESKTOP-K1B9ICQ\Wilmer <wilmer.guevara@epn.edu.ec>
2017-07-01 22:06:02 -0500

    Afinamiento

---
+++ KnockknockServerHilo/src/knockknockserverhilo/Conexion.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knockknockserverhilo;
+
+/*
+ * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+import java.net.*;
+import java.io.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Conexion extends Thread {
+
+    private Socket socket = null;
+    private Integer cliente;
+    private File file;
+    private ArrayList <String> datosEntrada;
+    public Conexion(Socket socket, Integer cliente, File file) {
+        super("Conexion");
+        this.socket = socket;
+        this.cliente = cliente;
+        this.file = file;
+    }
+
+    private static String printUsage(String mensaje) {
+        String resultados = "";
+        for (int i = 0; i < mensaje.length(); i++) {
+            int dato = mensaje.codePointAt(i);
+            dato += 4;
+            resultados += String.valueOf((char) dato);
+        }
+        return resultados;
+    }
+
+    public void run() {
+
+        try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));) {
+            FileWriter bufferedWriter;
+            File fileUsuarios = new File("C:\\Users\\Wilmer\\Documents\\Usuarios.txt");
+            FileReader fileReader = new FileReader(fileUsuarios);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String ip = "";
+            System.out.println("archivo:"+bufferedReader.readLine());
+            System.out.println("Socket:"+this.socket.getInetAddress().toString());
+            if ((ip = bufferedReader.readLine()) != null && ip.equals(this.socket.getInetAddress().toString())) {
+                
+                bufferedWriter = new FileWriter(file);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date iniciaConexion = Calendar.getInstance().getTime();
+                System.out.println("Hola " + this.cliente + " Ip: " + this.socket.getInetAddress() + " Tiempo " + dateFormat.format(iniciaConexion));
+                String datos = ("Cliente " + this.cliente + " Ip: " + this.socket.getInetAddress() + " Tiempo " + dateFormat.format(iniciaConexion) + "\t\n");
+
+                String inputLine, outputLine;
+                KnockKnockProtocol kkp = new KnockKnockProtocol();
+                outputLine = kkp.processInput(null);
+                out.println(outputLine);
+
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println(inputLine);
+                    datosEntrada.add(datos);
+                    datos += inputLine;
+                    datos += this.printUsage(inputLine);
+                    outputLine = kkp.processInput(inputLine);
+                    out.println(outputLine);
+                    if (outputLine.equals("Bye")) {
+                        break;
+                    }
+                }
+                for (int i = 0; i < 3; i++) {
+                 System.out.println(datosEntrada.get(i));
+                  
+                }
+                printWriter.println(datos);
+                bufferedWriter.close();
+                printWriter.close();
+                Date finConexion = Calendar.getInstance().getTime();
+                Long tiempo = finConexion.getTime() - iniciaConexion.getTime();
+                bufferedWriter = new FileWriter(file, true);
+                PrintWriter printWriter2 = new PrintWriter(bufferedWriter);
+                printWriter.println("Conexion Cliente " + this.cliente + " duro: " + tiempo / 60);
+                bufferedWriter.close();
+                printWriter.close();
+                printWriter2.close();
+            }else
+            System.out.println("no entro");
+            
+            fileReader.close();
+            bufferedReader.close();
+            socket.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ KnockknockServerHilo/src/knockknockserverhilo/KnockKnockServer.java
@@ -0,0 +1,77 @@
+package knockknockserverhilo;
+
+/*
+ * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import Base.MySQL;
+public class KnockKnockServer {
+
+    public static Integer contadorClientes = 0;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+//        if (args.length != 1) {
+ //       System.err.println("Usage: java KnockKnockServer <port number>");
+//            System.exit(1);
+//        }
+/**
+ * conexion a la base
+ */
+        MySQL db = new MySQL();
+        db.MySQLConnect();
+       
+        int portNumber = 4445;// Integer.parseInt(args[0]);
+        boolean listening = true;
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            contadorClientes++;
+            while (listening) {
+                System.out.println("Contador clientes en el servidor:" + contadorClientes);
+                if (contadorClientes <= 10) {
+                    new Conexion(serverSocket.accept(), contadorClientes, new File("log" + contadorClientes + ".txt")).start();
+                    incrementCount();
+                } else {
+                    while (contadorClientes > 10) {
+//                        serverSocket.
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+
+    public static synchronized void incrementCount() {
+        contadorClientes++;
+    }
+}
